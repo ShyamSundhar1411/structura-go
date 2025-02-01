a2e33b68da36881e4e0b7aa4df998d6d65379180
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2025-02-01 23:03:28 +0530

    chore: initialized based init command structure

--- cmd/init.go
+++ cmd/init.go
@@ -1,27 +1,37 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/manifoldco/promptui"
 	"github.com/spf13/cobra"
 )
+
+type Project struct {
+	Name string
+	Path string
+	Description string
+	Architecture string
+}
 
 // initCmd represents the init command
 var initCmd = &cobra.Command{
 	Use:   "init",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Initialize a new Go project structure",
+	Long: `A tool to quickly initialize a new Go project with common architectures
+	like MVC, MVCS, etc. It creates the necessary directories based on your
+	chosen architecture.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("init called")
+		var project Project
+		project.Name = interactivePrompt("Project Name", "my-project")
+		project.Path = interactivePrompt("Project Path", "./"+project.Name)
+		project.Description = interactivePrompt("Project Description", "A new Go project")
+		project.Architecture = selectArchitecture()
+		fmt.Print("Project '%s' initialized successfully at '%s'\n", project.Name, project.Path)
 	},
 }
 
@@ -37,4 +47,33 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// initCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	initCmd.Flags().StringP("name", "n", "", "Name of the project")
+	initCmd.Flags().StringP("path", "p", "", "Path to initialize the project")
+	initCmd.Flags().StringP("description", "d", "", "Description of the project")
+	initCmd.Flags().StringP("architecture", "a", "", "Architecture to use (MVC, MVCS, etc.)")
+}
+func interactivePrompt(label, defaultValue string) string {
+	prompt := promptui.Prompt{
+		Label:   label,
+		Default: defaultValue,
+	}
+	result, err := prompt.Run()
+	if err != nil{
+		fmt.Println("Error during generation",err)
+		os.Exit(1)
+	}
+	return result
+}
+
+func selectArchitecture()string{
+	prompt := promptui.Select{
+		Label: "Select Architecture",
+		Items: []string{"MVC", "MVCS", "MVP", "MVPF", "MVI"},
+	}
+	_, result, err := prompt.Run()
+	if err != nil{
+		fmt.Println("Error during generation", err)
+		os.Exit(1)
+	}
+	return result
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,13 @@ module github.com/ShyamSundhar1411/structura-go
 go 1.23.4
 
 require (
+	github.com/manifoldco/promptui v0.9.0
+	github.com/spf13/cobra v1.8.1
+)
+
+require (
+	github.com/chzyer/readline v1.5.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/spf13/cobra v1.8.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.29.0 // indirect
 )
