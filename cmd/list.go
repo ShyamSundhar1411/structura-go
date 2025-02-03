e8e23aca8d93b914c175d9d7d0116fc7471222e2
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2025-02-04 02:22:43 +0530

    feat: added list command

---
+++ cmd/list.go
@@ -0,0 +1,45 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// listCmd represents the list command
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List available architectures",
+	Long: `The 'list' command displays all available backend architectures 
+	that can be used to generate project folder structures. This helps you 
+	select the appropriate architecture for your Go project setup.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		architectures, err := loadAllTemplates("./templates/")
+		if err != nil {
+			fmt.Println("❌ Error loading templates:", err)
+			return
+		}
+		fmt.Println("📌 Available Architectures:")
+		for _, tmpl := range architectures {
+			fmt.Printf("🔹 %s: %s\n", tmpl.Architecture, tmpl.Description)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(listCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// listCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// listCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
--- cmd/promptUtils.go
+++ cmd/promptUtils.go
@@ -3,9 +3,17 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/manifoldco/promptui"
+	"gopkg.in/yaml.v3"
 )
+
+type Template struct {
+	Architecture string `yaml:"architecture"`
+	Description string `yaml:"description"`
+	Folders [] string `yaml:"folders"`
+}
 
 func selectPrompt(label string, options []string) string {
 	prompt := promptui.Select{
@@ -31,4 +39,30 @@ func interactivePrompt(label, defaultValue string) string {
 		os.Exit(1)
 	}
 	return result
+}
+
+func loadAllTemplates(dir string) ([]Template, error) {
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+	var templates []Template
+	for _,file := range files{
+		if (!strings.HasSuffix(file.Name(), ".yaml") || file.Name() == "initial_structure.yaml"){
+			continue
+		}
+		data,err := os.ReadFile(dir + "/" + file.Name())
+		if err != nil {
+			fmt.Println("⚠️ Error reading:", file.Name())
+			continue
+		}
+		var tmpl Template
+		err = yaml.Unmarshal(data, &tmpl)
+		if err != nil {
+			fmt.Println("⚠️ Error unmarshalling:", file.Name())
+			continue
+		}
+		templates = append(templates, tmpl)
+	}
+	return templates,nil
 }
